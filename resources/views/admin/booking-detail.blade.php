af2eaf9273c21a4903b7e78c0a92ef2a0770086a
Nguyễn Mai Khanh <whyicantremember@gmail.com>
2025-12-16 22:16:45 +0700

    Sửa css chi tiết hóa đơn

--- resources/views/admin/booking-detail.blade.php
+++ resources/views/admin/booking-detail.blade.php
@@ -30,18 +30,23 @@
                                 <div class="x_content">
                                     <section class="content invoice">
                                         <!-- title row -->
-                                        <div class="row">
-                                            <div class="  invoice-header">
-                                                <h3>
-                                                    <img src="{{ asset('admin/assets/images/icon/icon_office.png') }}"
-                                                        alt=""
-                                                        style="margin-right: 10px">{{ $invoice_booking->title }}
-                                                    <small class="pull-right">Ngày:
-                                                        {{ date('d-m-Y', strtotime($invoice_booking->bookingDate)) }}</small>
-                                                </h3>
-                                            </div>
+                                        <div class="invoice-header-fixed">
+    <div class="invoice-title">
+        <img src="{{ asset('admin/assets/images/icon/icon_office.png') }}"
+            style="margin-right:10px">
+        {{ $invoice_booking->title }}
+    </div>
+
+    <div class="invoice-date">
+        Ngày: {{ date('d-m-Y', strtotime($invoice_booking->bookingDate)) }}
+    </div>
+</div>
+
+
+
+
+
                                             <!-- /.col -->
-                                        </div>
                                         <!-- info row -->
                                         <div class="row invoice-info">
                                             <div class="col-sm-4 invoice-col">
